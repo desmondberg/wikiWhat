77fa4fb50222de80995567c7d32cdfa70a225354
dessie <lynxes.desmond@gmail.com>
2024-05-13 17:57:06 +0100

    fix footer link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="p-2 m-2">
         <div className="made-by">
-            <h3>made by <a className="text-orange-300 hover:text-orange-400" href='https://github.com/desmondbergen'>dezzy</a>, <a className="text-blue-300 hover:text-blue-400" href="https://en.wikipedia.org/wiki/Love_in_Us_All">Love in Us All</a></h3>
+            <h3>made by <a className="text-orange-300 hover:text-orange-400" href='https://github.com/desmondberg'>dezzy</a>, <a className="text-blue-300 hover:text-blue-400" href="https://en.wikipedia.org/wiki/Love_in_Us_All">Love in Us All</a></h3>
         </div>
     </footer>
   )
